72fa38a11c598ea1b5be98c47c30840579887fde
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-12-24 06:43:11 +0530

    Added validator for user models

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator');
+const { isLength } = require('lodash');
 const { signout, signup } = require("../controllers/auth")
 
 
-router.post("/signup", signup);
+router.post("/signup", [
+                check("name", "name should be at least 3 characters").isLength({ min : 3 }),
+                check("email", "email is required").isEmail(),
+                check("password", "password should be at least 5 characters", isLength({ min : 5 }) )
+        ], 
+        signup
+);
+
 router.get("/signout", signout);
 
 module.exports = router;
