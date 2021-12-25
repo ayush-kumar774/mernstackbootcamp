31109c37010e858c259c7f62f8b0cf29ad4a80e1
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-12-26 05:06:14 +0530

    Added custom error message for validation of signup

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,17 @@
 const User = require("../models/user");
+const { check, validatorResult, validationResult } = require("express-validator");
 
 exports.signup = (req, res) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty())
+        {
+                return res.status(422).json({
+                        error: errors.array()[0].msg
+                });
+        }
+
         const user = new User(req.body);
         user.save( (err, user) => {
                 if (err) {
@@ -11,7 +22,7 @@ exports.signup = (req, res) => {
                 res.json({
                        name : user.name,
                        email : user.email,
-                       id : user._idgit  
+                       id : user._id
                 });
         })
 };
