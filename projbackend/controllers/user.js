d8591d37acbfa8ffd61f36191be534e4b94a2696
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-12-30 17:06:26 +0530

    Assignment 1 Getting all users from DB

---
+++ projbackend/controllers/user.js
@@ -0,0 +1,36 @@
+const User = require("../models/user");
+
+exports.getUserById = (req, res, next, id) => {
+        User.findById(id).exec( (err, user) => {
+                if (err || !user)
+                {
+                        return res.status(400).json({
+                                error: "No user was found in DB"
+                        })
+                }
+
+                req.profile = user;
+                next();
+        } );
+};
+
+exports.getUser = (req, res) => {
+        //TODO: get back here for password
+        req.profile.salt = undefined;
+        req.profile.encry_password = undefined;
+        req.profile.createdAt = undefined;
+        req.profile.updatedAt = undefined;
+        req.profile.__v = undefined;
+        return res.json(req.profile);
+};
+
+exports.getAllUsers = (req, res) => {
+        User.find().exec( (err, users) => {
+                if (err || !users) {
+                        return res.status(400).json({
+                                error: "No users found"
+                        })
+                }
+                res.json(users);
+        } )
+}
