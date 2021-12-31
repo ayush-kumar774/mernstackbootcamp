1c9ae1e3e0b7ce007dbe0e5c0e65aadf6186178e
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-12-31 19:21:40 +0530

    Added function to update user details

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -15,7 +15,6 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
-        //TODO: get back here for password
         req.profile.salt = undefined;
         req.profile.encry_password = undefined;
         req.profile.createdAt = undefined;
@@ -23,6 +22,35 @@ exports.getUser = (req, res) => {
         req.profile.__v = undefined;
         return res.json(req.profile);
 };
+
+exports.updateUser = (req, res) => {
+        User.findByIdAndUpdate(
+                {
+                        _id: req.profile._id
+                },
+                {
+                        $set: req.body
+                },
+                {
+                        new: true, useFindAndModify: false
+                },
+                (err, user) => {
+                        if (err) {
+                                return res.status(400).json(
+                                        {
+                                                error: "Update in database is not successful"
+                                        }
+                                )
+                        }
+                        user.salt = undefined;
+                        user.encry_password = undefined;
+                        user.createdAt = undefined;
+                        user.updatedAt = undefined;
+                        user.__v = undefined;
+                        res.json(user);
+                }
+        )
+}
 
 
 
