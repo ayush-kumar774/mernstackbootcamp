94878a7361f3c6745fbc67d200660b7636be0ea4
ayush-kumar774 <ayushkumar1234@protonmail.com>
2022-01-05 08:11:41 +0530

    Adding create product function

---
+++ projbackend/controllers/product.js
@@ -0,0 +1,73 @@
+const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
+
+
+exports.getProductById = (req, res, next, id) => {
+        Product.findById(id)
+        .populate("category")
+        .exec( (err, product) => {
+                if (err) {
+                        return res.status(400).json({
+                                error: "Product not found"
+                        });
+                }
+                req.product = product;
+                next();
+        });
+};
+
+exports.createProduct = (req, res) => {
+        let form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+
+        form.parse(req, (err, fields, file) => {
+                if (err) {
+                        return res.status(400).json({
+                                error: "Problem with image"
+                   
+                        });
+                }
+
+                //destructure the field
+                const { name, description, price, category, stock } = fields;
+
+
+                if (
+                        !name ||
+                        !description ||
+                        !price ||
+                        !category ||
+                        !stock
+                ) {
+                        return res.status(400).json({
+                                error: "Please include all fields"
+                        });
+                }
+
+                let product = new Product(fields);
+
+
+                // handle file here
+                if (file.photo) {
+                        if (file.photo.size > 3000000) {
+                                return res.status(400).json({
+                                        error: "File size is too big!"
+                                });
+                        }
+                        product.photo.data = fs.readFileSync(file.photo.path);
+                        product.photo.contentType = file.photo.type;
+                }
+
+                //save to the DB
+                product.save((err, product) => {
+                        if (err) {
+                                res.status(400).json({
+                                        error: "Saving tshirt in DB failed"
+                                })
+                        }
+                        res.json(product)
+                })
+        });
+};
