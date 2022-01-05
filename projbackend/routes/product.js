9852312f6314879718392f0ceeea9a1942bf5087
ayush-kumar774 <ayushkumar1234@protonmail.com>
2022-01-05 16:17:08 +0530

    Added create, delete, read, update and listing routes for products

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const router = express.Router() ;
 
-const { getProductById, createProduct } = require("../controllers/product");
+const { 
+        getProductById, 
+        createProduct, 
+        getProduct, 
+        photo, 
+        deleteProduct, 
+        updateProduct,
+        getAllProducts
+ } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -11,7 +19,34 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 
-//all of actual routes
+// all of actual routes
+
+// create routes
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+
+// read route
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+// delete route
+router.delete(
+        "/product/:productId/:userId",
+        isSignedIn,
+        isAuthenticated,
+        isAdmin,
+        deleteProduct
+);
+
+// update route
+router.put(
+        "/product/:productId/:userId",
+        isSignedIn,
+        isAuthenticated,
+        isAdmin,
+        updateProduct
+);
+
+// listing route
+router.get("/products", getAllProducts);
 
 module.exports = router;
