eb0361910785e7711754c6214187599ca74c066f
ayush-kumar774 <ayushkumar1234@protonmail.com>
2022-01-06 06:22:45 +0530

    Added order route

--- projbackend/app.js
+++ projbackend/app.js
@@ -14,6 +14,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 
 // DB Connection
@@ -42,6 +43,7 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
 
 // PORT
 const port = process.env.PORT || 8000;
